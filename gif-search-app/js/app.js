d1029bfd3817ea70b26b7c101356c8438bb6bb75
Molly Stone <molly.stone89@gmail.com>
2019-04-12 16:20:01 -0400

    Create event handler to pull data into page and append to content

--- gif-search-app/js/app.js
+++ gif-search-app/js/app.js
@@ -1,1 +1,46 @@
-// console.log($);
+$(() => {
+    // =========================
+    // global variables
+    // =========================
+
+    const $mainContainer = $('.main-container');
+    const $userInput = $('input[type="text"]').val();
+
+    // =========================
+    // global functions
+    // =========================
+
+    // =========================
+    // event handlers
+    // =========================
+
+    // event handler for user keyword input on form submission
+    $('form').on('submit', (event) => {
+
+        // empty previous items from main div
+        $mainContainer.empty();
+
+        // keep page from reloading on form submission
+        event.preventDefault();
+
+        // reset form submission to be default (blank)
+        $(event.currentTarget).trigger('reset');
+
+        // get data based on user input. current results limited to 50 for now
+        let dataUrl = $.get(
+            "http://api.giphy.com/v1/gifs/search?q=" + $userInput + "&api_key=pdUgvuVVPEs9PdIYiNuPW8HrZYpNBm1P&limit=50"
+        );
+        dataUrl.done(
+            (data) => {
+                console.log('data successfully pulled', data);
+                for (let i = 0; i < 50; i++) {
+                    const $divGif = $('<div>').addClass('gif-container');
+                    const $divGifTitle = $('<div>').text(data.data[i].title);
+                    const $gif = $('<img>').attr('src', data.data[i].images.fixed_height_small.url);
+                    $divGif.append($divGifTitle);
+                    $divGif.append($gif);
+                    $mainContainer.append($divGif);
+                }
+        });
+    });
+});
