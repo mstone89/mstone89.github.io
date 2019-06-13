0c6b3c8dd48d6cb1794bc09c5bea85e13c0f325e
Molly Stone <molly.stone89@gmail.com>
2019-06-13 19:21:13 -0400

    Change browser router to hash router

--- molly-stone-portfolio/src/components/Sidebar.js
+++ molly-stone-portfolio/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Projects from './Projects';
 import Bio from './Bio';
