b8b23a84cf825afad419903618f3ac03f1454f6a
Molly Stone <molly.stone89@gmail.com>
2019-06-21 14:58:12 -0400

    Underline nav links when clicked

--- js/main.js
+++ js/main.js
@@ -10,21 +10,30 @@ $(() => {
     $resume = $('.resume');
 
     $projectsLink.on('click', () => {
+        $projectsLink.addClass('underline');
         $projects.removeClass('hide');
-        $bio = $('.bio').addClass('hide');
-        $resume = $('.resume').addClass('hide');
+        $bioLink.removeClass('underline');
+        $resumeLink.removeClass('underline');
+        $bio.addClass('hide');
+        $resume.addClass('hide');
     });
 
     $bioLink.on('click', () => {
+        $bioLink.addClass('underline');
         $projects.addClass('hide');
-        $bio = $('.bio').removeClass('hide');
-        $resume = $('.resume').addClass('hide');
+        $projectsLink.removeClass('underline');
+        $resumeLink.removeClass('underline');
+        $bio.removeClass('hide');
+        $resume.addClass('hide');
     });
 
     $resumeLink.on('click', () => {
+        $resumeLink.addClass('underline');
         $projects.addClass('hide');
-        $bio = $('.bio').addClass('hide');
-        $resume = $('.resume').removeClass('hide');
+        $bioLink.removeClass('underline');
+        $projectsLink.removeClass('underline');
+        $bio.addClass('hide');
+        $resume.removeClass('hide');
     });
 
 });
